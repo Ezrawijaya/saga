3d53ee39af9101ee4f0eb77ea3f87ac40df0f2af
Ezrawijaya <71765466+Ezrawijaya@users.noreply.github.com>
2020-09-23 19:35:12 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+  printf("Nama : Ezra Wijaya");
+  printf("\n");
+  
+  printf("NPM : 19081010054");
+  printf("\n");
+  
+  printf("Alamat : Citra Medayu Residence blok i-7");
+  printf("\n");
+  
+  printf("Fakultas : Fakultas Ilmu Komputer");
+  printf("\n");
+  
+  printf("Jurusan : Teknik Informatika");
+  printf("\n");
+  
+  
+  return 0;
+}
